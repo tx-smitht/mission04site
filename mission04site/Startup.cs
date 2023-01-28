1393e0c086a7c2b14b07f797ed6217bab26de213
tx-smitht <tx.smiththomas@gmail.com>
2023-01-27 19:29:47 -0700

    Add project files.

---
+++ mission04site/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission04site
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // This makes it load up the index.html
+            app.UseDefaultFiles();
+
+            // This makes it use the static files in the wwwroot
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ mission04site/wwwroot/js/script.js
@@ -0,0 +1,3 @@
+﻿$("#calculategrade").click(function () {
+    /*This program will be used to calculate the weighted grade average */
+})
