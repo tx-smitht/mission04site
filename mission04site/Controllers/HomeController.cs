fdd7ffefbbef0ad2a4801e374c7a2cabb9b26568
tx-smitht <tx.smiththomas@gmail.com>
2023-02-03 13:42:57 -0600

    adding comments

--- mission04site/Controllers/HomeController.cs
+++ mission04site/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ namespace mission04site.Controllers
 {
     public class HomeController : Controller
     {
+
+        // Render the index page
         public IActionResult Index()
         {
             return View();
         }
 
+        // Render the GradeCalc page
         public IActionResult GradeCalc()
         {
             return View();
